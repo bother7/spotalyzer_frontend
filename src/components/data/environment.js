5b4dcb4af83c1bb5545eadd27e961950ec43b681
bother7 <josephmcha@gmail.com>
2017-10-26 10:20:05 -0400

    i think

--- src/components/data/environment.js
+++ src/components/data/environment.js
@@ -1,2 +1,2 @@
 
-export const env_url = "postgres://azouetxtknvkif:ce09db6a3a96ee527763cb67992e56a55dcfc7519f2d30b511381ba8e1c50a8f@ec2-54-235-84-244.compute-1.amazonaws.com:5432/d5aoelkko8id9c/api/v1"
+export const env_url = "https://spotalyzer-backend.herokuapp.com/api/v1"
