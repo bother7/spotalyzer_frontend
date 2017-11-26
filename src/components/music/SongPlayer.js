fae430f59f09661beacc1538ada896dbb7104ddf
bother7 <josephmcha@gmail.com>
2017-11-26 15:40:09 -0500

    part 3

--- src/components/music/SongPlayer.js
+++ src/components/music/SongPlayer.js
@@ -4,11 +4,7 @@ import {connect} from 'react-redux'
 
 class SongContainer extends React.Component {
 
-  // handleClick = (event) => {
-  //   event.preventDefault()
-  //   console.log(helper.status)
-  //   helper.player.play('spotify:track:4uLU6hMCjMI75M1A2tKUQC');
-  // }
+
   componentDidMount(){
     this.refs.myIframe.onload = () => {
       console.log(this.refs.myIframe)
